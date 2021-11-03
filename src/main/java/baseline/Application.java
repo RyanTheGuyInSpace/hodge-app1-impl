eb15b45bbb4fad28923521d7d3b19f0e78116db9
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-11-02 23:08:21 -0400

    Updated launch code and start method

--- src/main/java/baseline/Application.java
+++ src/main/java/baseline/Application.java
@@ -5,8 +5,35 @@
 
 package baseline;
 
-public class Application {
-    public static void main(String[] args) {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Application extends javafx.application.Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("To-Do List");
+
+        button = new Button();
+        button.setText("New List");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Parent root = FXMLLoader.load(getClass().getResource("ToDoList.fxml"));
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
